62a3cd2568277e46dc4df6088c0113adbef03c6d
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2024-02-16 23:34:58 -0800

    Add TBD creator stats for collection struct

--- programs/mpl-asset/src/state/collection.rs
+++ programs/mpl-asset/src/state/collection.rs
@@ -8,4 +8,8 @@ pub struct CollectionData {
     pub owner: Pubkey,            //32
     pub name: String,             //4
     pub uri: String,              //4
+    // TBD creator stats:
+    pub num_minted: u64,
+    pub num_migrated: u64,
+    pub current_size: u64,
 }
