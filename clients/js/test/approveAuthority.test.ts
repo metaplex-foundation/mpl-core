db95b41ad99af6fd35e28572f0f44d3c96e6669b
Nhan Phan <nhan.phan@gmail.com>
2024-03-11 22:20:50 -0700

    approve auth, freeze tests

--- clients/js/test/approveAuthority.test.ts
+++ clients/js/test/approveAuthority.test.ts
@@ -7,6 +7,8 @@ import {
   updateAuthority,
   plugin,
   getPubkeyAuthority,
+  pluginAuthorityPair,
+  authority,
 } from '../src';
 import { DEFAULT_ASSET, assertAsset, createAsset, createUmi } from './_setup';
 
@@ -45,6 +47,152 @@ test('it can add an authority to a plugin', async (t) => {
       authority: {
         type: 'Pubkey',
         address: delegateAddress.publicKey,
+      },
+      frozen: false,
+    },
+  });
+});
+
+test('it can reassign authority of a plugin to another pubkey', async (t) => {
+  // Given a Umi instance and a new signer.
+  const umi = await createUmi();
+  const delegateAddress = generateSigner(umi);
+  const newDelegateAddress = generateSigner(umi);
+
+  const asset = await createAsset(umi, {
+    plugins: [pluginAuthorityPair({ type: 'Freeze', data: { frozen: false } })],
+  });
+
+  await assertAsset(t, umi, {
+    ...DEFAULT_ASSET,
+    asset: asset.publicKey,
+    owner: umi.identity.publicKey,
+    updateAuthority: updateAuthority('Address', [umi.identity.publicKey]),
+    freeze: {
+      authority: {
+        type: 'Owner',
+      },
+      frozen: false,
+    },
+  });
+
+  await approvePluginAuthority(umi, {
+    asset: asset.publicKey,
+    pluginType: PluginType.Freeze,
+    newAuthority: getPubkeyAuthority(delegateAddress.publicKey),
+  }).sendAndConfirm(umi);
+
+  await assertAsset(t, umi, {
+    ...DEFAULT_ASSET,
+    asset: asset.publicKey,
+    owner: umi.identity.publicKey,
+    updateAuthority: updateAuthority('Address', [umi.identity.publicKey]),
+    freeze: {
+      authority: {
+        type: 'Pubkey',
+        address: delegateAddress.publicKey,
+      },
+      frozen: false,
+    },
+  });
+
+  await approvePluginAuthority(umi, {
+    asset: asset.publicKey,
+    pluginType: PluginType.Freeze,
+    newAuthority: getPubkeyAuthority(newDelegateAddress.publicKey),
+  }).sendAndConfirm(umi);
+
+  await assertAsset(t, umi, {
+    ...DEFAULT_ASSET,
+    asset: asset.publicKey,
+    owner: umi.identity.publicKey,
+    updateAuthority: updateAuthority('Address', [umi.identity.publicKey]),
+    freeze: {
+      authority: {
+        type: 'Pubkey',
+        address: newDelegateAddress.publicKey,
+      },
+      frozen: false,
+    },
+  });
+});
+
+test('it can approve to reassign authority back to owner', async (t) => {
+  // Given a Umi instance and a new signer.
+  const umi = await createUmi();
+  const delegateAddress = generateSigner(umi);
+
+  const asset = await createAsset(umi, {
+    plugins: [pluginAuthorityPair({ type: 'Freeze', data: { frozen: false } })],
+  });
+
+  await approvePluginAuthority(umi, {
+    asset: asset.publicKey,
+    pluginType: PluginType.Freeze,
+    newAuthority: getPubkeyAuthority(delegateAddress.publicKey),
+  }).sendAndConfirm(umi);
+
+  await assertAsset(t, umi, {
+    ...DEFAULT_ASSET,
+    asset: asset.publicKey,
+    owner: umi.identity.publicKey,
+    updateAuthority: updateAuthority('Address', [umi.identity.publicKey]),
+    freeze: {
+      authority: {
+        type: 'Pubkey',
+        address: delegateAddress.publicKey,
+      },
+      frozen: false,
+    },
+  });
+
+  await approvePluginAuthority(umi, {
+    asset: asset.publicKey,
+    pluginType: PluginType.Freeze,
+    newAuthority: authority('Owner'),
+  }).sendAndConfirm(umi);
+
+  await assertAsset(t, umi, {
+    ...DEFAULT_ASSET,
+    asset: asset.publicKey,
+    owner: umi.identity.publicKey,
+    updateAuthority: updateAuthority('Address', [umi.identity.publicKey]),
+    freeze: {
+      authority: {
+        type: 'Owner',
+      },
+      frozen: false,
+    },
+  });
+});
+
+test('it cannot add invalid authorities to a plugin', async (t) => {
+  // Given a Umi instance and a new signer.
+  const umi = await createUmi();
+  const delegateAddress = generateSigner(umi);
+
+  const asset = await createAsset(umi, {
+    plugins: [pluginAuthorityPair({ type: 'Freeze', data: { frozen: false } })],
+  });
+
+  const result = approvePluginAuthority(umi, {
+    asset: asset.publicKey,
+    pluginType: PluginType.Freeze,
+    newAuthority: authority('Permanent', {
+      address: delegateAddress.publicKey,
+    }),
+  }).sendAndConfirm(umi);
+
+  await t.throwsAsync(result, { name: 'InvalidAuthority' });
+
+  await assertAsset(t, umi, {
+    ...DEFAULT_ASSET,
+    asset: asset.publicKey,
+    owner: umi.identity.publicKey,
+    updateAuthority: updateAuthority('Address', [umi.identity.publicKey]),
+    freeze: {
+      authority: {
+        type: 'Owner',
       },
       frozen: false,
     },
