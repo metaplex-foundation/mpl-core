c327cdc43295fc8e4afd232b55d844206119ec18
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-03-14 20:56:59 +0200

    rust hooked expand

--- clients/rust/src/hooked/mod.rs
+++ clients/rust/src/hooked/mod.rs
@@ -32,6 +32,7 @@ impl From<&Plugin> for PluginType {
             Plugin::UpdateDelegate(_) => PluginType::UpdateDelegate,
             Plugin::PermanentFreeze(_) => PluginType::PermanentFreeze,
             Plugin::Attributes(_) => PluginType::Attributes,
+            Plugin::PermanentTransfer(_) => PluginType::PermanentTransfer,
         }
     }
 }
