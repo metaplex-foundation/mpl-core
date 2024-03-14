6da967242bbca8e959aeacb9cc305e422e702acf
shotgunofdeath <faust.child@gmail.com>
2024-03-14 18:49:34 +0100

    feat(legacy helpers): add legacyDelegate and legacyRevoke helpers

---
+++ clients/js/src/instructions/errors.ts
@@ -0,0 +1,4 @@
+export const ERR_CANNOT_DELEGATE =
+  'Cannot delegate. The target delegate is already either a plugin authority or the asset owner';
+export const ERR_CANNOT_REVOKE =
+  'Cannot revoke. Either no plugins defined or the plugin authority is already the asset owner';
---
+++ clients/js/test/legacyRevoke.test.ts
@@ -0,0 +1,144 @@
+import test from 'ava';
+import { generateSigner } from '@metaplex-foundation/umi';
+import { assertAsset, createAsset, createUmi } from './_setup';
+import { pluginAuthorityPair, pubkeyPluginAuthority } from '../src';
+import { legacyRevoke } from '../src/instructions/legacyRevoke';
+import { ERR_CANNOT_REVOKE } from '../src/instructions/errors';
+
+test('it can revoke with one plugin defined', async (t) => {
+  // Given an Umi instance and asset with one required plugin with delegated authority.
+  const umi = await createUmi();
+  const delegateToRevokeFrom = generateSigner(umi);
+  const asset = await createAsset(umi, {
+    plugins: [
+      pluginAuthorityPair({
+        type: 'BurnDelegate',
+        authority: pubkeyPluginAuthority(delegateToRevokeFrom.publicKey),
+      }),
+    ],
+  });
+
+  // The authority of all defined plugins becomes the asset owner.
+  await legacyRevoke(umi, asset).sendAndConfirm(umi);
+
+  await assertAsset(t, umi, {
+    asset: asset.publicKey,
+    owner: umi.identity.publicKey,
+    updateAuthority: { type: 'Address', address: umi.identity.publicKey },
+    burnDelegate: {
+      authority: {
+        type: 'Owner',
+      },
+    },
+  });
+});
+
+test('it can revoke with all required plugins defined', async (t) => {
+  // Given an Umi instance and asset with all required plugins with delegated authorities.
+  const umi = await createUmi();
+  const delegateToRevokeFrom = generateSigner(umi);
+  const asset = await createAsset(umi, {
+    plugins: [
+      pluginAuthorityPair({
+        type: 'FreezeDelegate',
+        data: { frozen: false },
+        authority: pubkeyPluginAuthority(delegateToRevokeFrom.publicKey),
+      }),
+      pluginAuthorityPair({
+        type: 'TransferDelegate',
+        authority: pubkeyPluginAuthority(delegateToRevokeFrom.publicKey),
+      }),
+      pluginAuthorityPair({
+        type: 'BurnDelegate',
+        authority: pubkeyPluginAuthority(delegateToRevokeFrom.publicKey),
+      }),
+    ],
+  });
+
+  // The authority of all defined plugins becomes the asset owner.
+  await legacyRevoke(umi, asset).sendAndConfirm(umi);
+
+  await assertAsset(t, umi, {
+    asset: asset.publicKey,
+    owner: umi.identity.publicKey,
+    updateAuthority: { type: 'Address', address: umi.identity.publicKey },
+    freezeDelegate: {
+      authority: {
+        type: 'Owner',
+      },
+      frozen: false,
+    },
+    transferDelegate: {
+      authority: {
+        type: 'Owner',
+      },
+    },
+    burnDelegate: {
+      authority: {
+        type: 'Owner',
+      },
+    },
+  });
+});
+
+test('it cannot revoke if no plugins defined', async (t) => {
+  // Given an Umi instance and asset with no plugins defined.
+  const umi = await createUmi();
+  const asset = await createAsset(umi);
+
+  // The revoke error is expected.
+  t.throws(() => legacyRevoke(umi, asset), {
+    message: ERR_CANNOT_REVOKE,
+  });
+});
+
+test('it cannot revoke if one of plugin authorities is the asset owner', async (t) => {
+  // Given an Umi instance and asset with all required plugins defined, and all the plugins except one have a delegated authority.
+  const umi = await createUmi();
+  const delegateToRevokeFrom = generateSigner(umi);
+  const asset = await createAsset(umi, {
+    plugins: [
+      pluginAuthorityPair({
+        type: 'FreezeDelegate',
+        data: { frozen: false },
+      }),
+      pluginAuthorityPair({
+        type: 'TransferDelegate',
+        authority: pubkeyPluginAuthority(delegateToRevokeFrom.publicKey),
+      }),
+      pluginAuthorityPair({
+        type: 'BurnDelegate',
+        authority: pubkeyPluginAuthority(delegateToRevokeFrom.publicKey),
+      }),
+    ],
+  });
+
+  // The revoke error is expected.
+  t.throws(() => legacyRevoke(umi, asset), {
+    message: ERR_CANNOT_REVOKE,
+  });
+});
+
+test('it cannot revoke if all of the plugin authorities are the asset owner', async (t) => {
+  // Given an Umi instance and asset with all required plugins defined with the asset owner as the plugin authority.
+  const umi = await createUmi();
+  const asset = await createAsset(umi, {
+    plugins: [
+      pluginAuthorityPair({
+        type: 'FreezeDelegate',
+        data: { frozen: false },
+      }),
+      pluginAuthorityPair({
+        type: 'TransferDelegate',
+      }),
+      pluginAuthorityPair({
+        type: 'BurnDelegate',
+      }),
+    ],
+  });
+
+  // The revoke error is expected.
+  t.throws(() => legacyRevoke(umi, asset), {
+    message: ERR_CANNOT_REVOKE,
+  });
+});
