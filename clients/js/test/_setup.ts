56564ebf63258de72b6b3484d74f85e12c547d13
shotgunofdeath <faust.child@gmail.com>
2024-03-20 12:33:33 +0100

    feat(das): add das api

--- clients/js/test/_setup.ts
+++ clients/js/test/_setup.ts
@@ -1,5 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { createUmi as basecreateUmi } from '@metaplex-foundation/umi-bundle-tests';
+import {
+  createUmi as basecreateUmi,
+  testPlugins,
+} from '@metaplex-foundation/umi-bundle-tests';
 import { Assertions } from 'ava';
 import {
   PublicKey,
@@ -7,7 +10,9 @@ import {
   Umi,
   generateSigner,
   publicKey,
+  createUmi as baseCreateUmi,
 } from '@metaplex-foundation/umi';
+import { dasApi } from '@metaplex-foundation/digital-asset-standard-api';
 import {
   DataState,
   Key,
@@ -24,6 +29,8 @@ import {
 } from '../src';
 
 export const createUmi = async () => (await basecreateUmi()).use(mplCore());
+export const createUmiWithDas = (endpoint: string) =>
+  baseCreateUmi().use(testPlugins(endpoint)).use(dasApi());
 
 export type CreateAssetHelperArgs = {
   owner?: PublicKey | Signer;
@@ -48,6 +55,8 @@ export const DEFAULT_COLLECTION = {
   name: 'Test Collection',
   uri: 'https://example.com/collection',
 };
+
+export const DAS_API_ENDPOINT = process.env.DAS_API_ENDPOINT!;
 
 export const createAsset = async (
   umi: Umi,
