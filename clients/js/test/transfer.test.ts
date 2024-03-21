5cd0e341e661797bb6579d4fc05448b0297b941b
blockiosaurus <blockiosaurus@gmail.com>
2024-03-21 16:51:08 -0400

    Adding two more negative tests.

--- clients/js/test/transfer.test.ts
+++ clients/js/test/transfer.test.ts
@@ -166,3 +166,103 @@ test('authorities on owner-managed plugins are reset on transfer', async (t) => 
     },
   });
 });
+
+test('authorities on permanent plugins should not be reset on transfer', async (t) => {
+  // Given a Umi instance and a new signer.
+  const umi = await createUmi();
+  const freezeDelegate = generateSigner(umi);
+  const newOwner = generateSigner(umi);
+
+  const asset = await createAsset(umi, {
+    plugins: [
+      pluginAuthorityPair({
+        type: 'PermanentFreezeDelegate',
+        data: { frozen: false },
+        authority: pluginAuthority("Pubkey", { address: freezeDelegate.publicKey }),
+      }),
+    ],
+  });
+
+  await assertAsset(t, umi, {
+    asset: asset.publicKey,
+    owner: umi.identity.publicKey,
+    updateAuthority: { type: 'Address', address: umi.identity.publicKey },
+    permanentFreezeDelegate: {
+      authority: {
+        type: 'Pubkey',
+        address: freezeDelegate.publicKey,
+      },
+      offset: BigInt(119),
+      frozen: false,
+    },
+  });
+
+  await transferV1(umi, {
+    asset: asset.publicKey,
+    newOwner: newOwner.publicKey,
+  }).sendAndConfirm(umi);
+
+  await assertAsset(t, umi, {
+    asset: asset.publicKey,
+    owner: newOwner.publicKey,
+    updateAuthority: { type: 'Address', address: umi.identity.publicKey },
+    permanentFreezeDelegate: {
+      authority: {
+        type: 'Pubkey',
+        address: freezeDelegate.publicKey,
+      },
+      offset: BigInt(119),
+      frozen: false,
+    },
+  });
+});
+
+test('authorities on authority-managed plugin should not be reset on transfer', async (t) => {
+  // Given a Umi instance and a new signer.
+  const umi = await createUmi();
+  const delegate = generateSigner(umi);
+  const newOwner = generateSigner(umi);
+
+  const asset = await createAsset(umi, {
+    plugins: [
+      pluginAuthorityPair({
+        type: 'Attributes',
+        data: { attributeList: [] },
+        authority: pluginAuthority("Pubkey", { address: delegate.publicKey }),
+      }),
+    ],
+  });
+
+  await assertAsset(t, umi, {
+    asset: asset.publicKey,
+    owner: umi.identity.publicKey,
+    updateAuthority: { type: 'Address', address: umi.identity.publicKey },
+    attributes: {
+      authority: {
+        type: 'Pubkey',
+        address: delegate.publicKey,
+      },
+      offset: BigInt(119),
+      attributeList: [],
+    },
+  });
+
+  await transferV1(umi, {
+    asset: asset.publicKey,
+    newOwner: newOwner.publicKey,
+  }).sendAndConfirm(umi);
+
+  await assertAsset(t, umi, {
+    asset: asset.publicKey,
+    owner: newOwner.publicKey,
+    updateAuthority: { type: 'Address', address: umi.identity.publicKey },
+    attributes: {
+      authority: {
+        type: 'Pubkey',
+        address: delegate.publicKey,
+      },
+      offset: BigInt(119),
+      attributeList: [],
+    },
+  });
+});
