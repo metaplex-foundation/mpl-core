916e956ac731fe931c9924f470d8c5488f68b019
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2024-04-01 18:12:54 -0700

    Add another plugin authority reassign test case

--- clients/js/test/approveAuthority.test.ts
+++ clients/js/test/approveAuthority.test.ts
@@ -121,6 +121,76 @@ test('it cannot reassign authority of a plugin while already delegated', async (
   });
 });
 
+test('it cannot reassign authority of a plugin as delegate while already delegated', async (t) => {
+  // Given a Umi instance and a new signer.
+  const umi = await createUmi();
+  const delegateAddress = generateSigner(umi);
+  const newDelegateAddress = generateSigner(umi);
+
+  const asset = await createAsset(umi, {
+    plugins: [
+      pluginAuthorityPair({ type: 'FreezeDelegate', data: { frozen: false } }),
+    ],
+  });
+
+  await assertAsset(t, umi, {
+    ...DEFAULT_ASSET,
+    asset: asset.publicKey,
+    owner: umi.identity.publicKey,
+    updateAuthority: { type: 'Address', address: umi.identity.publicKey },
+    freezeDelegate: {
+      authority: {
+        type: 'Owner',
+      },
+      frozen: false,
+    },
+  });
+
+  await approvePluginAuthorityV1(umi, {
+    asset: asset.publicKey,
+    pluginType: PluginType.FreezeDelegate,
+    newAuthority: addressPluginAuthority(delegateAddress.publicKey),
+  }).sendAndConfirm(umi);
+
+  await assertAsset(t, umi, {
+    ...DEFAULT_ASSET,
+    asset: asset.publicKey,
+    owner: umi.identity.publicKey,
+    updateAuthority: { type: 'Address', address: umi.identity.publicKey },
+    freezeDelegate: {
+      authority: {
+        type: 'Address',
+        address: delegateAddress.publicKey,
+      },
+      frozen: false,
+    },
+  });
+
+  const result = approvePluginAuthorityV1(umi, {
+    asset: asset.publicKey,
+    pluginType: PluginType.FreezeDelegate,
+    authority: delegateAddress,
+    newAuthority: addressPluginAuthority(newDelegateAddress.publicKey),
+  }).sendAndConfirm(umi);
+
+  await t.throwsAsync(result, { name: 'CannotRedelegate' });
+
+  await assertAsset(t, umi, {
+    ...DEFAULT_ASSET,
+    asset: asset.publicKey,
+    owner: umi.identity.publicKey,
+    updateAuthority: { type: 'Address', address: umi.identity.publicKey },
+    freezeDelegate: {
+      authority: {
+        type: 'Address',
+        address: delegateAddress.publicKey,
+      },
+      frozen: false,
+    },
+  });
+});
+
+
 test('it cannot approve to reassign authority back to owner', async (t) => {
   // Given a Umi instance and a new signer.
   const umi = await createUmi();
