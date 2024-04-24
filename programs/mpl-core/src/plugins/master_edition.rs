32368e3c79e5a5915a492ad2dc17d35a06baba69
Nhan Phan <nhan.phan@gmail.com>
2024-04-23 22:10:50 -0700

    update comments

--- programs/mpl-core/src/plugins/master_edition.rs
+++ programs/mpl-core/src/plugins/master_edition.rs
@@ -2,12 +2,12 @@ use borsh::{BorshDeserialize, BorshSerialize};
 
 use super::PluginValidation;
 
-/// The edition plugin allows the creator to set an edition number on the asset
+/// The master edition plugin allows the creator to specify details on the master edition including max supply, name, and uri.
 /// The default authority for this plugin is the creator.
 #[repr(C)]
 #[derive(Clone, BorshSerialize, BorshDeserialize, Default, Debug, PartialEq, Eq)]
 pub struct MasterEdition {
-    /// The edition number.
+    /// The max supply of editions
     pub max_supply: u32,
     /// optional master edition name
     pub name: Option<String>,
