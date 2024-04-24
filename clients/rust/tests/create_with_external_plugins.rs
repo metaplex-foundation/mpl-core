e4b41b4275d9179d49f907d5c7a132fdd151bb56
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2024-04-24 12:16:15 -0700

    Add Oracle validation type

--- clients/rust/tests/create_with_external_plugins.rs
+++ clients/rust/tests/create_with_external_plugins.rs
@@ -101,6 +101,7 @@ async fn test_add_oracle() {
                     ExternalCheckResult { flags: 1 },
                 )]),
                 pda: None,
+                results_offset: None,
             })],
         },
     )
--- programs/mpl-core/src/plugins/oracle.rs
+++ programs/mpl-core/src/plugins/oracle.rs
@@ -2,8 +2,8 @@ use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{program_error::ProgramError, pubkey::Pubkey};
 
 use super::{
-    Authority, ExternalCheckResult, ExtraAccount, HookableLifecycleEvent, PluginValidation,
-    PluginValidationContext, ValidationResult,
+    Authority, ExternalCheckResult, ExternalValidationResult, ExtraAccount, HookableLifecycleEvent,
+    PluginValidation, PluginValidationContext, ValidationResult,
 };
 
 /// Oracle plugin that allows getting a `ValidationResult` for a lifecycle event from an arbitrary
@@ -16,6 +16,8 @@ pub struct Oracle {
     pub base_address: Pubkey,
     /// Optional PDA (derived from Pubkey attached to `ExternalPluginKey`).
     pub pda: Option<ExtraAccount>,
+    /// Validation results offset in the Oracle account.  Default is `ValidationResultsOffset::NoOffset`.
+    pub results_offset: ValidationResultsOffset,
 }
 
 impl From<&OracleInitInfo> for Oracle {
@@ -23,6 +25,10 @@ impl From<&OracleInitInfo> for Oracle {
         Self {
             base_address: init_info.base_address,
             pda: init_info.pda.clone(),
+            results_offset: init_info
+                .results_offset
+                .clone()
+                .unwrap_or(ValidationResultsOffset::NoOffset),
         }
     }
 }
@@ -39,6 +45,9 @@ pub struct OracleInitInfo {
     pub lifecycle_checks: Option<Vec<(HookableLifecycleEvent, ExternalCheckResult)>>,
     /// Optional PDA (derived from Pubkey attached to `ExternalPluginKey`).
     pub pda: Option<ExtraAccount>,
+    /// Optional offset for validation results struct used in Oracle account.  Default
+    /// is `ValidationResultsOffset::NoOffset`.
+    pub results_offset: Option<ValidationResultsOffset>,
 }
 
 impl PluginValidation for OracleInitInfo {
@@ -57,4 +66,35 @@ pub struct OracleUpdateInfo {
     pub lifecycle_checks: Option<Vec<(HookableLifecycleEvent, ExternalCheckResult)>>,
     /// Optional PDA (derived from Pubkey attached to `ExternalPluginKey`).
     pub pda: Option<ExtraAccount>,
+    /// Optional offset for validation results struct used in Oracle account.  Default
+    /// is `ValidationResultsOffset::NoOffset`.
+    pub results_offset: Option<ValidationResultsOffset>,
+}
+
+/// Offset to where the validation results struct is located in an Oracle account.
+#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, Eq, PartialEq)]
+pub enum ValidationResultsOffset {
+    /// The validation struct is located at the beginning of the account.
+    NoOffset,
+    /// The Oracle is an Anchor account so the validation struct is located after an 8-byte
+    /// account discriminator.
+    Anchor,
+    /// The validation struct is located at the specified offset within the account.
+    Custom(usize),
+}
+
+/// Validation results struct for an Oracle account.
+#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, Eq, PartialEq)]
+pub enum OracleValidation {
+    /// Version 1 of the format.
+    V1 {
+        /// Validation for the the create lifecycle action.
+        create: ExternalValidationResult,
+        /// Validation for the transfer lifecycle action.
+        transfer: ExternalValidationResult,
+        /// Validation for the burn lifecycle action.
+        burn: ExternalValidationResult,
+        /// Validation for the update lifecycle action.
+        update: ExternalValidationResult,
+    },
 }
