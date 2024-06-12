3114fa201bb307769b167d60dcf9acd6d20a1ef5
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2024-06-11 22:38:04 -0700

    Update JS SDK V1 to use updateV2

--- clients/js/src/instructions/update.ts
+++ clients/js/src/instructions/update.ts
@@ -1,21 +1,21 @@
 import { Context } from '@metaplex-foundation/umi';
 import {
   CollectionV1,
-  updateV1,
   AssetV1,
-  UpdateV1InstructionDataArgs,
+  UpdateV2InstructionDataArgs,
+  updateV2,
 } from '../generated';
 import { findExtraAccounts } from '../plugins';
 import { deriveExternalPluginAdapters } from '../helpers';
 
 export type UpdateArgs = Omit<
-  Parameters<typeof updateV1>[1],
+  Parameters<typeof updateV2>[1],
   'asset' | 'collection' | 'newName' | 'newUri'
 > & {
   asset: Pick<AssetV1, 'publicKey' | 'owner' | 'oracles' | 'lifecycleHooks'>;
   collection?: Pick<CollectionV1, 'publicKey' | 'oracles' | 'lifecycleHooks'>;
-  name?: UpdateV1InstructionDataArgs['newName'];
-  uri?: UpdateV1InstructionDataArgs['newUri'];
+  name?: UpdateV2InstructionDataArgs['newName'];
+  uri?: UpdateV2InstructionDataArgs['newUri'];
 };
 
 export const update = (
@@ -38,7 +38,7 @@ export const update = (
     }
   );
 
-  return updateV1(context, {
+  return updateV2(context, {
     ...args,
     asset: asset.publicKey,
     collection: collection?.publicKey,
