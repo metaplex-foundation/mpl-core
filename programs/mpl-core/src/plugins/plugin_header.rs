dc0053eea17b5234c57eea920a1c916d1b997c3f
VolodymyrBg <aqdrgg19@gmail.com>
2025-01-18 12:56:11 +0200

    Update plugin_header.rs

--- programs/mpl-core/src/plugins/plugin_header.rs
+++ programs/mpl-core/src/plugins/plugin_header.rs
@@ -1,12 +1,14 @@
 use crate::state::{DataBlob, Key, SolanaAccount};
 use borsh::{BorshDeserialize, BorshSerialize};
 use shank::ShankAccount;
+use bytemuck;
 
 /// The plugin header is the first part of the plugin metadata.
 /// This field stores the Key
 /// And a pointer to the Plugin Registry stored at the end of the account.
 #[repr(C)]
 #[derive(Clone, BorshSerialize, BorshDeserialize, Debug, ShankAccount)]
+#[derive(bytemuck::Pod, bytemuck::Zeroable)]
 pub struct PluginHeaderV1 {
     /// The Discriminator of the header which doubles as a Plugin metadata version.
     pub key: Key, // 1
